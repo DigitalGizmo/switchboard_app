9fa1e1f6e9ab819eaab6fa1f0950f03ea3cfc039
Don Button <donpublic@digitalgizmo.com>
2021-11-10 09:58:32 -0500

    either line connects the first call

--- src/App.svelte
+++ src/App.svelte
@@ -2,7 +2,19 @@
 	import HowTo from "./HowTo.svelte";
 	import Panel from "./Panel.svelte";
 
-	let audioCaption = 'ipsum dolor';
+	let audioCaption = '';
+	let caller = [null, null]; // row, col
+	let callersName = 'caller not yet identified';
+	let callee = [null, null]; // row, col
+	// one end engaged, other end engaged for each line
+	let lineStates = [[false, false], [false, false]];
+
+	const conversations = [
+		[[0,1],[1,2]], // Charlie call Olive
+		[[1,4], [0,3]] // Mina calls fire department
+	];
+	let currConvo = 0;
+	let prevConvo = null;
 
 	// $: uppercaseName = name.toUpperCase();
 	// $: console.log(name);
@@ -11,15 +23,131 @@
 	// 	age = 24;
 	// }
 
-	// function incrementAge() {
-	// 	age += 1;
-	// }
 	function startActivity() {
-		audioCaption = "Charlie's line <strong>flashing</strong>"
+		audioCaption = "Charlie's line <strong>flashing</strong> <br />"
+		// First conversation is first pair in first set
+		caller =  conversations[currConvo][0]; // [0,1];
+		// Set "target", person being called
+		callee 	 		 =  conversations[currConvo][1]
+		console.log('caller: ' + caller);
+		// Light Charlie
+		setFlashing(caller)
+		// Set one end of this line engaged
 	}
+
+	function setFlashing(caller) {
+		// Set caller row and column
+		jacks[caller[0]][caller[1]].ledState = 1;		
+	}
+
+	// --- Handle plug-in ----
+	// plugged in the form of [row, col, lineNum]
+	function identifyPlugged(plugged) {
+		// Get name based on row and col
+		callersName = jacks[plugged[0]][plugged[1]].name;
+		// Debug msg
+		audioCaption += 'plugged into: ' + callersName + '<br />';
+		// console.log(plugged);
+		// console.log(caller);
+
+		if (lineStates[plugged[2][0]]) { // one end already plugged
+			// Determing correct plugin for second end
+			// if row and column of plugged matches that of callee
+			if (plugged[0] === callee[0] && plugged[1] === callee[1]) {
+				console.log('got to 2nd plug equal');
+				// Turn led green
+				jacks[plugged[0]][plugged[1]].ledState = 2;
+				// Set first end of this line as in-use
+				// pluged[2] is the line index
+				lineStates[plugged[2][1]] = true;
+				// Debug message
+				audioCaption += callersName + ' on line: ' + plugged[2];
+			}
+		} else { // this is the first plugin for this line
+			// Determing correct plugin
+			// if row and column of plugged matches that of caller
+			if (plugged[0] === caller[0] && plugged[1] === caller[1]) {
+				// console.log('got to equal');
+				// Turn led green
+				jacks[plugged[0]][plugged[1]].ledState = 2;
+				// Set first end of this line as in-use
+				// pluged[2] is the line index
+				lineStates[plugged[2][0]] = true;
+				// Debug message
+				audioCaption += callersName + ' on line: ' + plugged[2] + 
+					' asks for 72 (Olive) <br />';
+			}
+		}
+	}
+
+	// setInterval(myTimer, 1000);
+
+	// function myTimer() {
+	//   const d = new Date();
+	//   document.getElementById("demo").innerHTML = d.toLocaleTimeString();
+	// }	
 	// function newName(event) {
 	// 	name = event.target.value;
 	// }
+
+  // Jacks
+  let jacks = [
+    [
+      { index: 0,
+        name: 'Dr. Freeman',
+        number: '82',
+        ledState: 0, 
+      },
+      { index: 1,
+        name: 'Charlie',
+        number: '56',
+        ledState: 0, 
+      },
+      { index: 2,
+        name: 'Tom',
+        number: '94',
+        ledState: 0, 
+      },
+      { index: 3,
+        name: 'Chief Burns',
+        number: '71',
+        ledState: 0, 
+      },
+      { index: 4,
+        name: 'Tressa',
+        number: '32',
+        ledState: 0, 
+      },
+    ],
+    [
+      { index: 5,
+        name: 'Emma',
+        number: '53',
+        ledState: 0, 
+      },
+      { index: 6,
+        name: 'Mrs. White',
+        number: '51',
+        ledState: 0, 
+      },
+      { index: 7,
+        name: 'Olive',
+        number: '72',
+        ledState: 0, 
+      },
+      { index: 8,
+        name: 'Sally',
+        number: '59',
+        ledState: 0, 
+      },
+      { index: 9,
+        name: 'Mina',
+        number: '24',
+        ledState: 0, 
+      },
+    ]
+  ]
+
 </script>
 
 <style>
@@ -37,6 +165,9 @@
 	<HowTo 
 		{audioCaption}
 	/>
-	<Panel />
+	<Panel 
+		{jacks}
+		{identifyPlugged}
+	/>
 </div><!-- /wrapper -->
 
--- src/Panel.svelte
+++ src/Panel.svelte
@@ -1,7 +1,10 @@
 <script>
   import { onMount } from 'svelte';
   import * as d3 from 'd3';
-  
+
+  export let jacks;
+  export let identifyPlugged;
+
   let name = 'Testing';
 
   // Plugs
@@ -11,22 +14,30 @@
   const INTER_PLUG_DELTA = 500; // Space between setsw
   const BODY_LINE_OFFSET_X = 42; // From plug left to line
   const BODY_LINE_OFFSET_Y = 160; // From plug top to line
+  let elID = 0;
 
   let plugs = [
-    { index: 0, 
+    { index: 0,
+      lineIndex: 0,
       x: PLUG_START_X, 
       y: PLUG_START_Y },
     { index: 1, 
+      lineIndex: 0,
       x: PLUG_START_X + INTRA_PLUG_DELTA, 
       y: PLUG_START_Y },
     { index: 2, 
+      lineIndex: 1,
       x: PLUG_START_X + INTER_PLUG_DELTA, 
       y: PLUG_START_Y },
     { index: 3, 
+      lineIndex: 1,
       x: PLUG_START_X + INTER_PLUG_DELTA + INTRA_PLUG_DELTA, 
       y: PLUG_START_Y},
   ]
 
+  const plugLineClases = ['line1', 'line1', 'line2', 'line2'];
+
+  // Phone Lines
   // For 1st line 1st plug
   $: x11 = plugs[0].x + BODY_LINE_OFFSET_X;
   $: y11 = plugs[0].y + BODY_LINE_OFFSET_Y;
@@ -61,91 +72,34 @@
   const ROW_OFFSETS = [0, 300];
   const JACK_TOP_OFFSET = 191 + 20;
 
-  // Jacks
-  let jacks = [
-    [
-      { index: 0,
-        name: 'Olive',
-        number: '87',
-        isLit: false, 
-      },
-      { index: 1,
-        name: 'Charlie',
-        number: '92',
-        isLit: true, 
-      },
-      { index: 2,
-        name: 'Tom',
-        number: '94',
-        isLit: false, 
-      },
-      { index: 3,
-        name: 'Chief Burns',
-        number: '71',
-        isLit: false, 
-      },
-      { index: 4,
-        name: 'Tressa',
-        number: '32',
-        isLit: false, 
-      },
-    ],
-    [
-      { index: 5,
-        name: 'Emma',
-        number: '53',
-        isLit: false, 
-      },
-      { index: 6,
-        name: 'Mrs. White',
-        number: '51',
-        isLit: false, 
-      },
-      { index: 7,
-        name: 'Dr. Freeman',
-        number: '82',
-        isLit: false, 
-      },
-      { index: 8,
-        name: 'Mina',
-        number: '24',
-        isLit: false, 
-      },
-      { index: 9,
-        name: 'Sally',
-        number: '59',
-        isLit: false, 
-      },
-    ]
-  ]
 
 
   onMount(() => {
     d3.selectAll('.plug').style('cursor', 'move')
     .call(d3.drag()
       .on("drag", function(d) {
-          // console.log('d.x: ' + d.x);
-          let elName = d3.select(this).attr("name");
-          // console.log('name: ' + elName);
-          plugs[elName].x = d.x -40;
-          plugs[elName].y = d.y -80;
+          elID = d3.select(this).attr("id");
+          plugs[elID].x = d.x -40;
+          plugs[elID].y = d.y -80;
 
        }).on("end", function (d){
-          // console.log('ended drag at: ' + d.x);
-          let elName = d3.select(this).attr("name");
           // Calculate closest row
           let proportion_of_total_height = d.y/PANEL_HEIGHT;
           let which_row_index = Math.trunc(proportion_of_total_height * NUM_ROWS);
           // Snap plug to calculated row
-          plugs[elName].y = (which_row_index * CELL_HEIGHT) + JACK_TOP_OFFSET;
+          plugs[elID].y = (which_row_index * CELL_HEIGHT) + JACK_TOP_OFFSET;
           // Calculate closeest column
           let proportion_of_total = d.x/PANEL_WIDTH;
           let which_col_index = Math.trunc(proportion_of_total * NUM_COLS);
           // Snap plug to calcuated x
-          plugs[elName].x = (which_col_index * CELL_WIDTH) + (CELL_WIDTH/2) - 55;
+          plugs[elID].x = (which_col_index * CELL_WIDTH) + (CELL_WIDTH/2) - 55;
           console.log('col: ' + which_col_index +
             ' row: ' + which_row_index);
           console.log('name: ' + jacks[which_row_index][which_col_index].name);
+          // Identify line
+          // which_line = d.index < 2 ? 1 : 2;
+          identifyPlugged([which_row_index, which_col_index, plugs[elID].lineIndex]);
+          // identifyPlugged([which_row_index, which_col_index]);
 
        }) // end on end
     ); // end call drag
@@ -154,63 +108,61 @@
 </script>
 
 <style>
-.plug-body {
-  fill:#4D4D4D;
-}
+  .plug-body {
+    fill:#4D4D4D;
+  }
 
-/*//PLUGS//*/
+  /*//PLUGS//*/
 
-.rounded-line {
-  /*stroke:#4D4D4D;*/
-  /*stroke:red;*/
-  stroke-width:14;
-  stroke-linecap:round;
-  stroke-miterlimit:10;
-}
+  .rounded-line {
+    /*stroke:#4D4D4D;*/
+    /*stroke:red;*/
+    stroke-width:14;
+    stroke-linecap:round;
+    stroke-miterlimit:10;
+  }
 
-.line1 {
-  stroke:#EE4A4A;
-}
+  .line1 {
+    stroke:#EE4A4A;
+  }
 
-.line2 {
-  stroke:#4A4AEE;
-}
+  .line2 {
+    stroke:#4A4AEE;
+  }
 
-.plug-line {
-  stroke:#4D4D4D;
-}
+/*  .plug-line {
+    stroke:#4D4D4D;
+  }
 
-    
-.label {
-  fill:#FFFFFF;
-  font-family:'PTSans-Bold';
-}
+*/      
+  .label {
+    fill:#FFFFFF;
+    font-family:'PTSans-Bold';
+  }
 
-/* from global css*/
+  /* from global css*/
 
-.led-light {
-  fill:gray;
-  stroke:#919090;
-  stroke-width:3;
-  stroke-miterlimit:10;
-}
+  .led-light {
+    fill:gray;
+    stroke:#919090;
+    stroke-width:3;
+    stroke-miterlimit:10;
+  }
 
-.led-light-green {
-  fill:#64BC46;
-  stroke:#919090;
-}
+  .led-green {
+    fill:#64BC46;
+    stroke:#919090;
+  }
 
-.led-light-red {
-  fill:red;
-  stroke:#919090;
-}
-
+  .led-red {
+    fill:red;
+    stroke:#919090;
+  }
 
 </style>
 
 <div id="panel">
-
-  <!-- Static sockets -->
+  <!-- sockets -->
   <svg id="switchboard-grid" class="sockets" version="1.1" xmlns="http://www.w3.org/2000/svg" 
     xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
     viewBox="0 0 1420 1200" xml:space="preserve">
@@ -223,7 +175,8 @@
           <text x="50" y="50" fill="black">{k.name} <tspan>-</tspan>{k.number}</text>
           <circle 
             class="led-light"
-            class:led-light-red="{k.isLit}"
+            class:led-green="{k.ledState === 2}"
+            class:led-red="{k.ledState === 1}"
             cx="127" 
             cy="105.9" 
             r="33.5"/>
@@ -233,46 +186,46 @@
       {/each}
     {/each}
 
+    <!-- hard coded jacks -->
+      <g transform="translate(0, 600)" class="unavailable">
+        <rect width="250" height="275"/>
+        <text x="100" y="50" fill="black">Ben</text>
+        <text x="100" y="75" fill="black">476</text>
+       <rect x="102" y="116" width="50" height="50" fill="#919090"/>
+        <circle cx="126.5" cy="141.5" r="7.5" fill="black"/>
+      </g>
 
-    <g transform="translate(0, 600)" class="unavailable">
-      <rect width="250" height="275"/>
-      <text x="100" y="50" fill="black">Ben</text>
-      <text x="100" y="75" fill="black">476</text>
-     <rect x="102" y="116" width="50" height="50" fill="#919090"/>
-      <circle cx="126.5" cy="141.5" r="7.5" fill="black"/>
-    </g>
+      <g transform="translate(275, 600)" class="available">
+        <rect width="250" height="275"/>
+        <text x="100" y="50" fill="black">Sue</text>
+        <text x="100" y="75" fill="black">989</text>
+        <rect x="102" y="116" width="50" height="50" fill="#919090"/>
+        <circle cx="126.5" cy="141.5" r="7.5" fill="black"/>
+      </g>
 
-    <g transform="translate(275, 600)" class="available">
-      <rect width="250" height="275"/>
-      <text x="100" y="50" fill="black">Sue</text>
-      <text x="100" y="75" fill="black">989</text>
-      <rect x="102" y="116" width="50" height="50" fill="#919090"/>
-      <circle cx="126.5" cy="141.5" r="7.5" fill="black"/>
-    </g>
+      <g transform="translate(550, 600)" class="available">
+        <rect width="250" height="275"/>
+        <text x="100" y="50" fill="black">Ann</text>
+        <text x="100" y="75" fill="black">977</text>
+        <rect x="102" y="116" width="50" height="50" fill="#919090"/>
+        <circle cx="126.5" cy="141.5" r="7.5" fill="black"/>
+      </g>
 
-    <g transform="translate(550, 600)" class="available">
-      <rect width="250" height="275"/>
-      <text x="100" y="50" fill="black">Ann</text>
-      <text x="100" y="75" fill="black">977</text>
-      <rect x="102" y="116" width="50" height="50" fill="#919090"/>
-      <circle cx="126.5" cy="141.5" r="7.5" fill="black"/>
-    </g>
+      <g transform="translate(825, 600)" class="available">
+        <rect width="250" height="275"/>
+        <text x="100" y="50" fill="black">Sue</text>
+        <text x="100" y="75" fill="black">989</text>
+        <rect x="102" y="116" width="50" height="50" fill="#919090"/>
+        <circle cx="126.5" cy="141.5" r="7.5" fill="black"/>
+      </g>
 
-    <g transform="translate(825, 600)" class="available">
-      <rect width="250" height="275"/>
-      <text x="100" y="50" fill="black">Sue</text>
-      <text x="100" y="75" fill="black">989</text>
-      <rect x="102" y="116" width="50" height="50" fill="#919090"/>
-      <circle cx="126.5" cy="141.5" r="7.5" fill="black"/>
-    </g>
-
-    <g transform="translate(1100, 600)" class="available">
-      <rect width="250" height="275"/>
-      <text x="100" y="50" fill="black">Ann</text>
-      <text x="100" y="75" fill="black">977</text>
-      <rect x="102" y="116" width="50" height="50" fill="#919090"/>
-      <circle cx="126.5" cy="141.5" r="7.5" fill="black"/>
-    </g>    
+      <g transform="translate(1100, 600)" class="available">
+        <rect width="250" height="275"/>
+        <text x="100" y="50" fill="black">Ann</text>
+        <text x="100" y="75" fill="black">977</text>
+        <rect x="102" y="116" width="50" height="50" fill="#919090"/>
+        <circle cx="126.5" cy="141.5" r="7.5" fill="black"/>
+      </g>    
 
     <!-- plug base -->
     <g transform="translate(0, 1090)">
@@ -280,21 +233,26 @@
     </g>
 
     {#each plugs as d}
-      <g transform="translate({d.x}, {d.y})" class="plug" name="{d.index}">
-        <line class="rounded-line plug-line" x1="42" y1="-20" x2="42" y2="160"/>
+      <g 
+        transform="translate({d.x}, {d.y})" 
+        class="{d.lineIndex === 0 ? 'plug line1' : 'plug line2'}" 
+        id="{d.index}">
+        <line 
+          class="{d.index < 2 ? 'rounded-line line1' : 'rounded-line line2'}"  
+          x1="42" y1="-20" x2="42" y2="160"/>
         <rect width="84" height="120" class="plug-body"/>
         <text class="label" x="42" y="60" text-anchor="middle">PLUG IN</text> 
       </g> 
     {/each}
-    <g> <!-- first set of lines -->
-      <line class="rounded-line line1" x1="{x11}" y1="{y11}" x2="{x12}" y2="{y2}"/>
-      <line class="rounded-line line1" x1="{x21}" y1="{y21}" x2="{x22}" y2="{y2}"/>
-      <line class="rounded-line line1" x1="{x12}" y1="{y2}" x2="{x22}" y2="{y2}" />
+    <g class="line1"> <!-- first set of lines -->
+      <line class="rounded-line" x1="{x11}" y1="{y11}" x2="{x12}" y2="{y2}"/>
+      <line class="rounded-line" x1="{x21}" y1="{y21}" x2="{x22}" y2="{y2}"/>
+      <line class="rounded-line" x1="{x12}" y1="{y2}" x2="{x22}" y2="{y2}" />
     </g>
-    <g> <!-- second set of lines -->
-      <line class="rounded-line line2" x1="{x31}" y1="{y31}" x2="{x32}" y2="{y2}"/>
-      <line class="rounded-line line2" x1="{x41}" y1="{y41}" x2="{x42}" y2="{y2}"/>
-      <line class="rounded-line line2" x1="{x32}" y1="{y2}" x2="{x42}" y2="{y2}" />
+    <g class="line2"> <!-- second set of lines -->
+      <line class="rounded-line" x1="{x31}" y1="{y31}" x2="{x32}" y2="{y2}"/>
+      <line class="rounded-line" x1="{x41}" y1="{y41}" x2="{x42}" y2="{y2}"/>
+      <line class="rounded-line" x1="{x32}" y1="{y2}" x2="{x42}" y2="{y2}" />
     </g>
 
 
