01468dee6c6430d0c5362b63887abe6619461cc5
Don Button <donpublic@digitalgizmo.com>
2022-10-18 21:00:52 -0400

    reduce grid to 12

--- src/ProtoPanelHelper.js
+++ src/ProtoPanelHelper.js
@@ -1,24 +1,24 @@
 let rowColToIndex = [
   [
-    {personIdx: 8, isPlugged: false}, // name: 'no name',
+    // {personIdx: 8, isPlugged: false}, // name: 'no name',
     {personIdx: 0, isPlugged: false}, // 'Emma Powers - store'
     {personIdx: 1, isPlugged: false}, // 'Mrs. White',
     {personIdx: 2, isPlugged: false}, // 'Mina Newell' Paula Fowler?,
     {personIdx: 9, isPlugged: false}, //'Place Holder',
   ],
   [
-    {personIdx: 3, isPlugged: false}, // name: 'Charlie Freeman',
+    {personIdx: 5, isPlugged: false}, // 'Tressa Howe', 
     {personIdx: 10, isPlugged: false}, //'Place Holder',
     {personIdx: 4, isPlugged: false}, // 'Olive Powers',
-    {personIdx: 11, isPlugged: false}, //'Place Holder',
-    {personIdx: 5, isPlugged: false}, // 'Tressa Howe', 
+    // {personIdx: 11, isPlugged: false}, //'Place Holder',
+    {personIdx: 3, isPlugged: false}, // name: 'Charlie Freeman',
   ],
   [
     {personIdx: 12, isPlugged: false},  //'Place Holder',
     {personIdx: 6, isPlugged: false}, // name: 'Tom Libby',
     {personIdx: 13, isPlugged: false}, //'Place Holder',
     {personIdx: 7, isPlugged: false}, // name: 'Chief Aurthur Burns',
-    {personIdx: 14, isPlugged: false}, //'Place Holder',
+    // {personIdx: 14, isPlugged: false}, //'Place Holder',
   ]
 ];
 
