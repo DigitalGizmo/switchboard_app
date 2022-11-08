b77cad6e6a9a009e3186080af5a4a457344a0844
Juliet Jacobson <julietanna@users.noreply.github.com>
2022-11-08 15:06:48 -0500

    new layout, added sketch

--- src/HowTo.svelte
+++ src/HowTo.svelte
@@ -7,16 +7,17 @@
 </style>
 
   <div class="how-to">
+    <img src="https://dev.digitalgizmo.com/msm-ed/ed-assets/images/how-to-sketch.jpg">
     <h3>Your Job</h3>
-    <p><strong>To pick up a call:</strong><br/>
-    Drag the plug into the socket with the red flashing light.</p>
-    <p><strong>To connect a call:</strong><br/>
-    Drag the other end of the cable into the requested socket.</p>
+    <p><strong>To answer an incoming call:</strong><br/>
+    Drag the plug into the socket with the flashing light.</p>
+    <p><strong>To connect the caller to someone else:</strong><br/>
+    Drag the other end of the cable into the socket that the caller requests.</p>
     
     <h3>Key</h3>
     <ul>
-      <li class="ringing"> flashing light indicates an incoming call</li>
-      <li class="in-use"> steady light indicates a line is busy on a call</li>
+      <li> a flashing amber light indicates an incoming call</li>
+      <li> a steady amber light indicates a line is busy on a call</li>
     </ul>
 
   </div>
